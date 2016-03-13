b500ba22654ad9c8ccc8367af06a1e4b2f0d903c
enginekit <enginecollection@gmail.com>
2016-03-13 16:16:27 +0700

    13: renamed file AstToJson.cs  to ...
    AstCSharpToJsonVisitor

---
+++ ManualILSpy/ManualILSpy/Extention/AstCSharpToJsonVisitor_1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using ICSharpCode.Decompiler;
+using ICSharpCode.NRefactory;
+using ICSharpCode.NRefactory.CSharp;
+using ICSharpCode.NRefactory.PatternMatching;
+using ICSharpCode.NRefactory.TypeSystem;
+using ManualILSpy.Extention.Json;
+using System.Linq;
+
+namespace ManualILSpy.Extention
+{
+    partial class AstCSharpToJsonVisitor : IAstVisitor
+    {
+       
+    }
+}
